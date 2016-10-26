5c3e292fd2324a20aa00473d51acd1b86c51d52a
SSR666 <shezarsaifee@gmail.com>
2016-10-26 15:15:45 -0500

    Add files via upload

---
+++ module3/scripts/quiz.js
@@ -0,0 +1,28 @@
+function processForm()
+{
+	var numberOfCorrectResponses= 0;
+	
+	var q1 = document.forms.quiz1.question1;
+	
+	if (q1[1].checked)
+		{
+		numberOfCorrectResponses= numberOfCorrectResponses + 1;
+		}
+	
+	var q2 = document.forms.quiz1.question2;
+	
+	if (q2[0].checked)
+		{
+		numberOfCorrectResponses= numberOfCorrectResponses + 1;
+		}
+	
+	var q3 = document.forms.quiz1.question3;
+	
+	if (q3[0].checked)
+		{
+		numberOfCorrectResponses= numberOfCorrectResponses + 1;
+		}
+	
+	var score= 	Math.round(numberOfCorrectResponses / 3*100);
+	alert( "You recieved a " + score + "!" );
+}
